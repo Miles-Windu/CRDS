87bdc7d45ee86dbb38b94703b1ce4e8f18390152
Miles Kenner <mdk0109@yahoo.com>
2019-05-06 13:07:53 -0500

    made sure all buttons navigate through the server and not just load html pages

--- client/src/components/Network/Network.js
+++ client/src/components/Network/Network.js
@@ -7,7 +7,7 @@ class Network extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-light navbar-fixed-top">
             <div className="container justify-content-center">
-                <a href="index.html" className="pull-left"><img alt="" src={Image} /></a>  
+                <a href="/" className="pull-left"><img alt="" src={Image} /></a>  
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -15,16 +15,16 @@ class Network extends Component {
                         <div className="collapse navbar-collapse" id="myNavbar">
                             <ul className="navbar-nav mr-auto">
                                 <li className="nav-item">
-                                    <a className="nav-link" id="myBtn" href="./user.html">My Networks</a>
+                                    <a className="nav-link" id="myBtn" href="/user/network">My Networks</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" id="myBtn" href="./user.html">Edit My CRDS</a>
+                                    <a className="nav-link" id="myBtn" href="/user">Edit My CRDS</a>
                                 </li>
                             </ul>
                         <ul className="nav navbar-nav flex-fill  justify-content-end">
                             <div className="login">
                                 <li className="nav-item">
-                                    <button id="myBtn" type="button" className="btn btn-outline-info  my-2 my-sm-0 mr-sm-2 " href="./user.html" tabindex="-1"> Logout </button>
+                                    <button id="myBtn" type="button" className="btn btn-outline-info  my-2 my-sm-0 mr-sm-2 " href="/" tabindex="-1"> Logout </button>
                                 </li>
                             </div>
                         </ul>
