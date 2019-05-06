ed4713ef4bc46c020b04ed236b42bce7affc4a01
Miles Kenner <mdk0109@yahoo.com>
2019-05-06 12:48:27 -0500

    react router is working consistently

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-// import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import Register from "./components/Register/Register.js"
-// import User from "./components/User/User";
+import User from "./components/User/User";
 // import './App.css';
-import User from './components/User/User.js';
-// import Network from './components/Network/Network.js';
+import Network from './components/Network/Network.js';
 import Landing from "./components/Landing/Landing.js"
 
 function App() {
 
     return <div>
-      <Landing />
+      <Router>
+        <Route exact path="/" component={Landing} />
+        <Route path="/user" component={User} />
+        <Route path="/user/network" component={Network} />
+        <Route path="/register" component={Register} />
+      </Router>
     </div>
     
 }
--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -15,7 +15,7 @@ class Landing extends Component {
             <div>
             <nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top">
         <div class="container justify-content-center">
-            <a href="index.html" class="pull-left"><img src={image} alt=""/></a>  
+            <a href="/" class="pull-left"><img src={image} alt=""/></a>  
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
