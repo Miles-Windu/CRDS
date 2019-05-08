a2a479061e8d53c58510857166a699dcf212cbf9
estevansta <cruz@therrb.co>
2019-05-07 20:01:34 -0500

    small cosmetics and card ex additions

--- client/src/components/Network/Network.js
+++ client/src/components/Network/Network.js
@@ -6,6 +6,7 @@ import Image from "../images/Crds_white.png";
 import image2 from "../images/Crds_black.png";
 
 class Network extends Component {
+
     render(){
         return (
             <div>
@@ -40,7 +41,6 @@ class Network extends Component {
 
         {/* <!-- MY NETWORKS --> */}
 
-       
        <br />
        <div className="container overflow-auto justify-content-center container-fluid">
            <div className="card">
@@ -66,107 +66,155 @@ class Network extends Component {
                             <h3 id="list-item-1">Web/Technology</h3>
                             {/* <!-- CATEGORY CAPTION --> */}
                             <p>Consists of Web Developers, Software developers and those proficient in coding/logic</p>
-                            {/* <!-- CARD 1 DUMMY CARD --> */}
                             <div className="row">
-                            <div className=" col-lg-4 col-xl-6" id="user-card">
-                                <div className="card" style={{maxWidth: "600px"}}>
-                                    <div className="col-md-4 justify-content-center card-image waves-effect waves-block waves-light">
-                                    <br />
-                                    <img className="activator" src={image2} alt="" style={{width: "100px", height: "100px"}} />
-                                    </div>
-                                    <hr />
-                                    <div className="card-content">
-                                    <span className="card-title activator grey-text text-darken-4">Ferat Imrak</span>
-                                    <p>Full-Stack Web developer</p>
-                                    </div>
-                                    {/* <!-- RESPONSIVE ASPECT OF CARD --> */}
-                                    <div className="card-reveal">
-                                    <span className="card-title grey-text text-darken-4">Ferat Imrak<i className="material-icons right">close</i></span>
-                                        <p className="card-text"><small className="text-muted">Proficient in: HTML, CSS, Javascript, React.js, Node.js, mongoDB
-                                        </small></p>
-                                        <p className="card-text"> Phone Number: 1 (708) 123-4567</p>
-                                        <p className="card-text"> 405 Church St, Evanston, IL 60201</p>
-                                        <a href="https://github.com/jiro1/Project-3">Check out my website!</a>
-                                        
-                                        
-                                        <div class="addthis_inline_share_toolbox_e8kh"></div>
-            
-						
-            
-                                    </div>
-                                </div>
-                            </div>
-                            {/* <!-- CARD 2 DUMMY CARD --> */}
-                            <div className=" col-lg-4 col-xl-6" id="user-card">
-                                <div className="card" style={{maxWidth: "600px"}}>
-                                    <div className="col-md-4 justify-content-center card-image waves-effect waves-block waves-light">
-                                    <br />
-                                    <img className="activator" src={image2} alt="" style={{width: "100px", height: "100px"}} />
-                                    </div>
-                                    <hr />
-                                    <div className="card-content">
-                                    <span className="card-title activator grey-text text-darken-4">Diego M.S</span>
-                                    <p>React and API Specialist</p>
-                                    </div>
-                                    {/* <!-- RESPONSIVE ASPECT OF CARD --> */}
-                                    <div className="card-reveal">
-                                    <span className="card-title grey-text text-darken-4">Diego M.S<i className="material-icons right">close</i></span>
-                                        <p className="card-text"><small className="text-muted">Proficient in: HTML, CSS, Javascript, React.js, Node.js, mongoDB
-                                        </small></p>
-                                        <p className="card-text"> Phone Number: 1 (708) 123-4567</p>
-                                        <p className="card-text"> 405 Church St, Evanston, IL 60201</p>
-                                        <a href="https://github.com/jiro1/Project-3">Check out my website!</a>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* <!-- CARD 3 DUMMY CARD --> */}
-                            <div className=" col-lg-4 col-xl-6" id="user-card">
-                                <div className="card" style={{maxWidth: "600px"}}>
-                                    <div className="col-md-4 justify-content-center card-image waves-effect waves-block waves-light">
-                                    <br />
-                                    <img className="activator" src={image2} alt="" style={{width: "100px", height: "100px"}} />
-                                    </div>
-                                    <hr />
-                                    <div className="card-content">
-                                    <span className="card-title activator grey-text text-darken-4">Miles Windu</span>
-                                    <p>Jedi and React Master</p>
-                                    </div>
-                                    {/* <!-- RESPONSIVE ASPECT OF CARD --> */}
-                                    <div className="card-reveal">
-                                    <span className="card-title grey-text text-darken-4">Miles Windu<i className="material-icons right">close</i></span>
-                                        <p className="card-text"><small className="text-muted">Proficient in: HTML, CSS, Javascript, React.js, Node.js, mongoDB
-                                        </small></p>
-                                        <p className="card-text"> Phone Number: 1 (708) 123-4567</p>
-                                        <p className="card-text"> 405 Church St, Evanston, IL 60201</p>
-                                        <a href="https://github.com/jiro1/Project-3">Check out my website!</a>
+                            {/* INSERT CARDS HERE ***************************************************/}
+                                {/* <!-- CARD 1 DUMMY CARD --> */}
+                                <div className=" col-lg-4 col-xl-6" id="user-card">
+                                    <div className="card" style={{maxWidth: "600px"}}>
+                                        <div className="col-md-4 justify-content-center card-image waves-effect waves-block waves-light">
+                                        <br />
+                                        <img className="activator" src={image2} alt="" style={{width: "100px", height: "100px"}} />
+                                        </div>
+                                        <hr />
+                                        <div className="card-content">
+                                        <span className="card-title activator grey-text text-darken-4">Ferat Imrak</span>
+                                        <p>Full-Stack Web developer</p>
+                                        </div>
+                                        {/* <!-- RESPONSIVE ASPECT OF CARD --> */}
+                                        <div className="card-reveal">
+                                        <span className="card-title grey-text text-darken-4">Ferat Imrak<i className="material-icons right">close</i></span>
+                                            <p className="card-text"><small className="text-muted">Proficient in: HTML, CSS, Javascript, React.js, Node.js, mongoDB
+                                            </small></p>
+                                            <p className="card-text"> Phone Number: 1 (708) 123-4567</p>
+                                            <p className="card-text"> 405 Church St, Evanston, IL 60201</p>
+                                            <a href="https://github.com/jiro1/Project-3">Check out my website!</a>
+                                            {/* SHARE */}
+                                            <p>SHARING OPTIONS BELOW:</p>
+                                            <div class="addthis_inline_share_toolbox_e8kh"></div>
                                         </div>
                                     </div>
                                 </div>
-                            <div> 
+                                {/* <!-- CARD 2 DUMMY CARD --> */}
+                                <div className=" col-lg-4 col-xl-6" id="user-card">
+                                    <div className="card" style={{maxWidth: "600px"}}>
+                                        <div className="col-md-4 justify-content-center card-image waves-effect waves-block waves-light">
+                                        <br />
+                                        <img className="activator" src={image2} alt="" style={{width: "100px", height: "100px"}} />
+                                        </div>
+                                        <hr />
+                                        <div className="card-content">
+                                        <span className="card-title activator grey-text text-darken-4">Diego M.S</span>
+                                        <p>React and API Specialist</p>
+                                        </div>
+                                        {/* <!-- RESPONSIVE ASPECT OF CARD --> */}
+                                        <div className="card-reveal">
+                                        <span className="card-title grey-text text-darken-4">Diego M.S<i className="material-icons right">close</i></span>
+                                            <p className="card-text"><small className="text-muted">Proficient in: HTML, CSS, Javascript, React.js, Node.js, mongoDB
+                                            </small></p>
+                                            <p className="card-text"> Phone Number: 1 (708) 123-4567</p>
+                                            <p className="card-text"> 405 Church St, Evanston, IL 60201</p>
+                                            <a href="https://github.com/jiro1/Project-3">Check out my website!</a>
+                                            {/* SHARE */}
+                                            <p>SHARING OPTIONS BELOW:</p>
+                                            <div class="addthis_inline_share_toolbox_e8kh"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* <!-- CARD 3 DUMMY CARD --> */}
+                                <div className=" col-lg-4 col-xl-6" id="user-card">
+                                    <div className="card" style={{maxWidth: "600px"}}>
+                                        <div className="col-md-4 justify-content-center card-image waves-effect waves-block waves-light">
+                                        <br />
+                                        <img className="activator" src={image2} alt="" style={{width: "100px", height: "100px"}} />
+                                        </div>
+                                        <hr />
+                                        <div className="card-content">
+                                        <span className="card-title activator grey-text text-darken-4">Miles Windu</span>
+                                        <p>Jedi and React Master</p>
+                                        </div>
+                                        {/* <!-- RESPONSIVE ASPECT OF CARD --> */}
+                                        <div className="card-reveal">
+                                        <span className="card-title grey-text text-darken-4">Miles Windu<i className="material-icons right">close</i></span>
+                                            <p className="card-text"><small className="text-muted">Proficient in: HTML, CSS, Javascript, React.js, Node.js, mongoDB
+                                            </small></p>
+                                            <p className="card-text"> Phone Number: 1 (708) 123-4567</p>
+                                            <p className="card-text"> 405 Church St, Evanston, IL 60201</p>
+                                            <a href="https://github.com/jiro1/Project-3">Check out my website!</a>
+                                            {/* SHARE */}
+                                            <p>SHARING OPTIONS BELOW:</p>
+                                            <div class="addthis_inline_share_toolbox_e8kh"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <div> 
+
+                             {/* <!-- CATEGORY TITLE --> */}
+                             <h3 id="list-item-1">Media/Marketing</h3>
+                            <p> Need content for your business? Digital media, social media, and production for commericals are key in gaining following and expanding clientelle</p>
+                            <div className="row">
+                            {/* INSERT CARDS HERE ***************************************************/}
+                                {/* <!-- CARD 3 DUMMY CARD --> */}
+                                <div className=" col-lg-4 col-xl-6" id="user-card">
+                                <div className="card" style={{maxWidth: "600px"}}>
+                                    <div className="col-md-4 justify-content-center card-image waves-effect waves-block waves-light">
+                                    <br />
+                                    <img className="activator" src={image2} alt="" style={{width: "100px", height: "100px"}} />
+                                    </div>
+                                    <hr />
+                                    <div className="card-content">
+                                    <span className="card-title activator grey-text text-darken-4">Estevan Cruz</span>
+                                    <p>Photographer and Social Media Specialist</p>
+                                    </div>
+                                    {/* <!-- RESPONSIVE ASPECT OF CARD --> */}
+                                    <div className="card-reveal">
+                                    <span className="card-title grey-text text-darken-4">Estevan Cruz<i className="material-icons right">close</i></span>
+                                        <p className="card-text"><small className="text-muted">Skilled in Lifestyle, e-commerce, and event-based photography</small></p>
+                                        <p className="card-text"> Phone Number: 1 (708) 123-4567</p>
+                                        <p className="card-text"> 405 Church St, Evanston, IL 60201</p>
+                                        <p className="card-text"> I also run social media accounts and do social media take-over's</p>
+                                        <a href="https://github.com/jiro1/Project-3">Check out my website!</a>
+                                         {/* SHARE */}
+                                         <p>SHARING OPTIONS BELOW:</p>
+                                        <div class="addthis_inline_share_toolbox_e8kh"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                             {/* <!-- CATEGORY TITLE --> */}
                             <h3 id="list-item-1">Household/Carpentry</h3>
                             <p> Anywhere from Carpentry to interior design projects and home rennovation. Have a small moving business? or something similar to 1800-GOT-JUNK</p>
-                            {/* INSERT CARDS HERE */}
+                            <div className="row">
+                                 {/* INSERT CARDS HERE ***************************************************/}
+                            </div>
 
                              {/* <!-- CATEGORY TITLE --> */}
                             <h3 id="list-item-4">Law/Legal</h3>
                             <p> Legal advice is always needed, but legal advice from a mutual friend/contact is guaranteed more helpful than getting billed by the minute to talk to a big lawfirm</p>
-                            {/* INSERT CARDS HERE */}
+                            <div className="row">
+                                 {/* INSERT CARDS HERE ***************************************************/}
+                            </div>
 
                              {/* <!-- CATEGORY TITLE --> */}
                             <h3 id="list-item-5">Medical/Health</h3>
                             <p> Medical advice? or Freelance medical specialists? Having a contact in the field of medicie is almost like having an on-demand PubMed </p>
-                            {/* INSERT CARDS HERE */}
+                            <div className="row">
+                                 {/* INSERT CARDS HERE ***************************************************/}
+                            </div>
 
                              {/* <!-- CATEGORY TITLE --> */}
                             <h3 id="list-item-6">Accounting/Finance</h3>
                             <p> Taxes.. and financial responsibity is tough, make it easier by sharing your CPA's info with other's to help out </p>
-                            {/* INSERT CARDS HERE */}
+                            <div className="row">
+                                 {/* INSERT CARDS HERE ***************************************************/}
+                            </div>
 
                              {/* <!-- CATEGORY TITLE --> */}
                             <h3 id="list-item-7">Food/Service</h3>
                             <p> Having restaruant contact's is important! Especially for corporate catering and events! </p>
-                            {/* INSERT CARDS HERE */}
+                            <div className="row">
+                                 {/* INSERT CARDS HERE ****************************************************/}
+                            </div>
 
                         
                             </div>  
