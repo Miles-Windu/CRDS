10bd988dad7093adaa9a6bbd5325bede9341c833
estevansta <cruz@therrb.co>
2019-05-09 15:04:03 -0500

    landing page linked on app.src

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import User from "./components/User/User";
 import Network from './components/Network/Network.js';
 import fullprofile from './components/FullProfile/fullprofile.js';
 import Card from "./components/BusinessCard/Card";
+import Landing from "./components/Landing/Landing";
 
 class App extends Component {
 
